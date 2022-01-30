ae74e4925d27baf068e71f5796a790b342abaf96
ahsan <muhammad.ahsan@gmail.com>
2022-01-30 09:33:17 +0100

    Added triangle + diamond

--- shapes.py
+++ shapes.py
@@ -14,12 +14,36 @@ def right_triangle(width: int = 10) -> None:
         print('', sep='')
 
 
-def diamond(width: int = 10) -> None:
-    raise NotImplementedError('Wait for it')
+def triangle(width: int = 11) -> None:
+    center = int(width / 2)
+    for i in range(center):
+        for j in range(width):
+            if center - i <= j <= center + i:
+                print('*', sep='', end='')
+            else:
+                print(' ', sep='', end='')
+        print('', sep='')
+
+
+def diamond(width: int = 11) -> None:
+    center = int(width / 2)
+    for i in range(width):
+        for j in range(width):
+            if i <= center:
+                if center - i <= j <= center + i:
+                    print('*', sep='', end='')
+                else:
+                    print(' ', sep='', end='')
+            else:
+                if i - center <= j <= center + width - i -1:
+                    print('*', sep='', end='')
+                else:
+                    print(' ', sep='', end='')
+        print('', sep='')
 
 
 def main():
-    diamond()
+    diamond(21)
 
 
 if __name__ == '__main__':
