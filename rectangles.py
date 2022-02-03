51ee6a87efcbef17a2dc7db342802e57337850cf
ahsan <muhammad.ahsan@gmail.com>
2022-02-03 23:17:51 +0100

    Added further challenge details

--- rectangles.py
+++ rectangles.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass
 
 
+# Problem
+# Given a 2D dimensional space
+# A set point of given in 2D space
+# Count the number of rectangles
+# Example
+
+# (0,0), (0,1), (1,0), (1,1) -> 1
+# (0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2) -> 9
+# (0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2), (2,3) -> 9
+
 @dataclass
 class Point:
     x: int
