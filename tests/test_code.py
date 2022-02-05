845ee464adbc1b9d0a9bac79a866347ecc0189e2
ahsan <muhammad.ahsan@gmail.com>
2022-02-05 11:31:45 +0100

    refactor

--- tests/test_code.py
+++ tests/test_code.py
@@ -5,14 +5,22 @@ from challenge.rectangles import count_rectangles
 
 
 @pytest.fixture
-def get_square() -> typing.List[Vertex]:
+def square_vertices() -> typing.List[Vertex]:
     return [Vertex(0, 0), Vertex(0, 1), Vertex(1, 0), Vertex(1, 1)]
 
 
 @pytest.fixture
-def get_rectangle() -> typing.List[Vertex]:
+def rectangle_vertices() -> typing.List[Vertex]:
     return [Vertex(0, 0), Vertex(0, 2), Vertex(1, 0), Vertex(1, 2)]
 
 
-def test_count_rectangles(get_square):
-    assert count_rectangles(get_square) == 1
+def test_non_polygon():
+    assert count_rectangles(None) == 0
+    assert count_rectangles([]) == 0
+    assert count_rectangles([Vertex(0, 0)]) == 0
+    assert count_rectangles([Vertex(0, 0), Vertex(0, 0)]) == 0
+    assert count_rectangles([Vertex(0, 0), Vertex(0, 1), Vertex(1, 0)]) == 0
+
+
+def test_count_rectangles(square_vertices):
+    assert count_rectangles(square_vertices) == 1
