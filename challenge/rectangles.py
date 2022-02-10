c84195179ba3420f036142f36b544a8fc0eaa141
ahsan <muhammad.ahsan@gmail.com>
2022-02-10 23:22:42 +0100

    Buggy implementation

--- challenge/rectangles.py
+++ challenge/rectangles.py
@@ -1,4 +1,5 @@
 import typing
+from itertools import combinations
 from utils.vertex import Vertex
 
 
@@ -16,10 +17,22 @@ def count_rectangles(vertices: typing.List[Vertex]):
     if vertices is None or len(vertices) < 4:
         return 0
     # Implement the Logic
-    for vertex in vertices:
-        x,y = vertex
+    # Consider 2D plane with x-axis on horizontal line and y-axis at vertical lines
+# BUGGY
+    vindex = {}
+    hindex = {}
+    for v in vertices:
+        vindex.setdefault(v.x, []).append(v.y)
+        hindex.setdefault(v.y, []).append(v.x)
+    for _, y in vindex.items():
+        comb = list(combinations(y, 2))
+        count = 0
+        for i in comb:
+            if i in hindex.keys():
+                count += 1
 
-    return -1
+    result = count if count % 2 == 0 else count - 1
+    return result
 
 
 def main():
