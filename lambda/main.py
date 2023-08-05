287f8dc2a2a5c0b7432b24dc982008456ed45770
sediame <dounia.sediame@gmail.com>
2023-08-05 16:38:59 +0200

    using reduce functions

--- lambda/main.py
+++ lambda/main.py
@@ -2,6 +2,7 @@ def cube(x): return x ** 3
 
 
 def main():
+    numbers = [1, 2, 3, 4, 5]
     # pep-8 does not allow this
     cube = lambda x: x ** 3  # NOQA
     print(cube(2))
@@ -12,9 +13,18 @@ def main():
     # Anonymous function can be invoked like this
     print((lambda x: x ** 2)(5))
 
-    print(list(map(lambda x: x ** 2, [1, 2, 3])))
+    # Transformation operation
+    print(list(map(lambda x: x ** 2, numbers)))
+    # Transformation + Summation
+    print(sum(map(lambda x: x ** 2, numbers)))
 
-    print(sum(map(lambda x: x ** 2, [1, 2, 3])))
+    # Lambda for filter
+    even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+    print(even_numbers)
+
+    # Map + Reduce Operation
+    from functools import reduce
+    print(reduce(lambda x, y: x + y, [1]))
 
 
 if __name__ == '__main__':
