beb5e6222729a0abb2467d626f67b7c7ba9e73ac
sediame <dounia.sediame@gmail.com>
2023-08-05 21:16:21 +0200

    Moved code to be tested into usecases module

--- tests/usecases.py
+++ tests/usecases.py
@@ -1,4 +1,9 @@
-# Code used as use cases for software  testing
+import requests
+from requests import Response
+
+""" Code used as use cases for software  testing
+"""
+
 
 class Teacher:
     pass
@@ -23,3 +28,23 @@ class Classroom:
     def add_student(self, student: Student):
         self.student = student
         self.has_student = True
+
+
+def third_party_dependency() -> int:
+    url = "https://www.google.com"
+    try:
+        response: Response = requests.get(url, timeout=2.50)
+        return response.status_code
+    except requests.exceptions.HTTPError as http_error:
+        print("Http Error:", http_error)
+    except requests.exceptions.ConnectionError as connection_error:
+        print("Error Connecting:", connection_error)
+    except requests.exceptions.Timeout as timeout_error:
+        print("Timeout Error:", timeout_error)
+    except requests.exceptions.RequestException as request_error:
+        print("Oh No!: Something Else", request_error)
+    return -1
+
+
+def api_response() -> str:
+    return "success" if third_party_dependency() == 200 else "failure"
